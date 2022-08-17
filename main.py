2552df2aa4552c2053051d7db74e4652d6c1f9ad
Carlos Andres Correa Rua <ccorrear@sura.com.co>
2022-08-17 17:37:30 -0500

    se crean datos primitivos

---
+++ main.py
@@ -0,0 +1,16 @@
+# hola  me llami comentario de line
+'''
+hola soy un comentario de bloque
+
+'''
+# DATOS PRIMITIVOS SON DATOS BASICOS
+
+nombre="carlos"
+edad=33
+estatura= 1.83
+hinchadelmejor=True 
+
+#salidas print
+
+print("hola: ",nombre)
+print(f'hola {nombre}')
